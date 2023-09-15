5a30b278c4d69fba96ddc1bdb8c6ed29f13d7585
JanisHuser <28865968+JanisHuser@users.noreply.github.com>
2023-09-15 14:10:45 +0200

    lecture 01

---
+++ 01/01_Schulung.pyp
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="utf-8"?>
+<Element>
+    <Script>
+        <Name>ProEngineers\Schulung\PythonPart.py</Name>
+        <Title>Schulung</Title>
+        <Version>0.0.1.0</Version>
+        <Interactor>False</Interactor>
+    </Script>
+</Element>
---
+++ 01/Pythonpart.py
@@ -0,0 +1,56 @@
+import NemAll_Python_Geometry as Geometry
+import NemAll_Python_BaseElements as BaseElements
+import NemAll_Python_BasisElements as BasisElements
+
+VALID_ALLPLAN_VERSIONS = [2023, 2024]
+
+
+def check_allplan_version(build_ele, version):
+    """
+    Check the current Allplan version
+
+    Args:
+        build_ele: the building element.
+        version:   the current Allplan version
+
+    Returns:
+        True/False if version is supported by this script
+    """
+
+    # Delete unused arguments
+    del build_ele
+
+    print(version)
+
+    for item in VALID_ALLPLAN_VERSIONS:
+        if str(item) in version:
+            return True
+
+    # Disable not valid versions
+    return False
+
+
+def create_element(build_ele, doc):
+    """
+    Creation of element
+
+    Args:
+        build_ele: the building element.
+        doc:       input document
+    """
+
+    del build_ele
+    del doc
+
+    model_ele_list = []
+    handle_list = []
+
+    placement = Geometry.AxisPlacement3D()
+
+    com_prop = BaseElements.CommonProperties()
+    com_prop.GetGlobalProperties()
+
+    brep = Geometry.BRep3D.CreateCuboid(placement, 1000, 1000, 1000)
+    model_ele_list.append(BasisElements.ModelElement3D(com_prop, brep))
+
+    return (model_ele_list, handle_list)
